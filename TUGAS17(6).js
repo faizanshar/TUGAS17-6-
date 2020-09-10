b99b906f9f246f0fe60f81d0328f0deea82398f6
faizanshar <muhammadfaizanshar@gmai.com>
2020-09-10 22:34:55 +0700

    first commit

---
+++ TUGAS17(6).js
@@ -0,0 +1,9 @@
+var jualMobil = {
+    nama : 'Tompel',
+    merek :  'Avanza',
+    tahun : 2017,
+    iklan : function(){
+        document.write(`Bu,saya ${this.nama}, jual mobil ${this.merek}, tahun ${this.tahun}, bekas`)
+    }
+}
+jualMobil.iklan();
